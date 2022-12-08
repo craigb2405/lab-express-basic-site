e48d951b55b4472cb6ef1a573fafac1aa38dcf63
Craig Butler <craig.butler@jrbsolutions.co.uk>
2022-12-08 20:54:23 +0000

    Solved Lab

---
+++ app.js
@@ -0,0 +1,25 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res)=>{
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/gallery', (req, res)=>{
+    res.sendFile(__dirname + '/views/gallery.html')
+})
+
+app.listen(3000)
+
